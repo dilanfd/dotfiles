4afd1ef9ce7a04e7da184aed4258eada2f9e1376
Dilan Fernando <dilan.fd@gmail.com>
2017-12-09 16:17:43 -0600

    removed .emacs.d again

--- .emacs.d/elpa/rope-read-mode-20161003.610/rope-read-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rope-read-mode" "20161003.610" "Rearrange lines to read text smoothly" 'nil :url "https://github.com/marcowahl/rope-read-mode" :keywords '("reading" "convenience" "chill"))
